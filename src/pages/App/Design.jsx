b43871cfb248ac81b758ba33bad3b4bb3859f131
brendonvan <brendon@van.digital>
2022-10-13 18:07:23 -0700

    Complete Design Paint/Wheel/Interior options

--- src/pages/App/Design.jsx
+++ src/pages/App/Design.jsx
@@ -43,10 +43,48 @@ function Design(props) {
           
         </div>
         <div className="design-controls">
-          <div className="options"><div className="options-motor"></div></div>
-          <div className="options"><div className="options-paint"></div></div>
-          <div className="options"><div className="options-wheel"></div></div>
-          <div className="options"><div className="options-interior"></div></div>
+          <div className="options">
+            <div className="options-motor">
+              <p>Dual Motor All-Wheel Drive</p>
+              <div className="motor-option-1"><p>Tesla S</p><p>$96,590</p></div>
+              <p>Tri Motor All-Wheel Drive</p>
+              <div className="motor-option-2"><p>Plaid</p><p>$127,590</p></div>
+            </div>  
+          </div>
+          <div className="options">
+            <div className="options-paint">
+              <h2>Paint</h2>
+              <div className="paint-selection">
+                <img src="/icons/paint-pearl-white-multi-coat.png" alt="paint-pearl-white-multi-coat.png" />
+                <img src="/icons/paint-solid-black.png" alt="paint-solid-black.png" />
+                <img src="/icons/paint-midnight-silver-metallic.png" alt="paint-midnight-silver-metallic.png" />
+                <img src="/icons/paint-deep-blue-metalic.png" alt="paint-deep-blue-metalic.png" />
+                <img src="/icons/paint-red-multi-coat.png" alt="paint-red-multi-coat.png" />
+              </div>
+              <div className="paint-details"><p>Pearl White Multi-Coat</p><p>Included</p></div>
+            </div>
+          </div>
+          <div className="options">
+            <div className="options-wheel">
+              <h2>Wheel</h2>
+              <div className="wheel-selection">
+                <img src="/icons/wheel-19-tempest.png" alt="wheel-19-tempest.png" />
+                <img src="/icons/wheel-21-arachnid.png" alt="wheel-21-arachnid.png" />
+              </div>
+              <div className="wheel-details"><p>19" Tempest Wheels</p><p>Included</p></div>
+            </div>
+          </div>
+          <div className="options">
+            <div className="options-interior">
+              <h2>Interior</h2>
+              <div className="interior-selection">
+                <img src="/icons/interior-all-black.png" alt="interior-all-black.png" />
+                <img src="/icons/interior-black-and-white.png" alt="interior-black-and-white.png" />
+                <img src="/icons/interior-cream.png" alt="interior-cream.png" />
+              </div>
+              <div className="interior-details"><p>All Black</p><p>Included</p></div>
+            </div>
+          </div>
           <div className="options"><div className="options-enhanced-autopilot"></div></div>
           <div className="options"><div className="options-full-autopilot"></div></div>
         </div>
